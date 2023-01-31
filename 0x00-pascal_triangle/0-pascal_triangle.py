1efc7223be18b15f6da1bcedf5cd6b96089e7f7f
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-01-31 15:00:50 -0800

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,47 +16,21 @@ triangle that is a list of lists of integers and print the triangle in the forma
 And in the last if name == "main" block, it will call the print_triangle function and pass the pascal_triangle 
 function result as an argument.
 '''
-
 def pascal_triangle(n):
+    '''
+    function def pascal_triangle(n):
+    that returns a list of lists of integers
+    representing the Pascal’s triangle of n:
+    Returns an empty list if n <= 0
+    You can assume n will be always an integer
+    '''
+    triangle = []
     if n <= 0:
-        return []
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
+        triangle
+    else:
+        for i in range(n):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+            triangle.append(row)
     return triangle
-
-
-#!/usr/bin/python3
-"""0-pascal_triangle."""
-
-triangle = list()
-triangle.append([1])
-
-
-# def pascal_triangle(n):
-#     """
-#     Return a list of lists of intergers representing the Pascal's Triangle.
-
-#     temp is temporary list to be storing the elements of to manipulate /
-#     to form the new row.
-#     row will be every new row of the values of that row position.
-#     sumNextValue will be the sum of the values that forms the current row.
-
-#     Args:
-#         n (int): The number of rows of the triangle.
-#     """
-#     if n <= 0:
-#         return list()
-#     else:
-#         for i in range(n - 1):
-#             temp = [0] + triangle[-1] + [0]
-#             row = list()
-#             for j in range(len(triangle[-1]) + 1):
-#                 sumNextVal = temp[j] + temp[j + 1]
-#                 row.append(sumNextVal)
-#             triangle.append(row)
-#         return triangle
