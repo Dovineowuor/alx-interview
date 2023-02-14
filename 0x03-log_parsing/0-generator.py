ee000526f3b62147d9b65b62c760bdf231be52ec
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-02-15 02:47:25 +0300

    Update 0-generator.py

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,19 +1,15 @@
+#!/usr/bin/python3
 import random
-import time
+import sys
+from time import sleep
+import datetime
 
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-
-def generate_log_line():
-    ip_address = ".".join(str(random.randint(0, 255)) for i in range(4))
-    date = time.strftime("%d/%b/%Y:%H:%M:%S +0000", time.gmtime())
-    method = "GET"
-    endpoint = "/projects/260"
-    http_version = "HTTP/1.1"
-    status_code = str(random.choice(status_codes))
-    file_size = str(random.randint(0, 10000))
-    log_line = f"{ip_address} - [{date}] \"{method} {endpoint} {http_version}\" {status_code} {file_size}"
-    return log_line
-
-while True:
-    print(generate_log_line())
-    time.sleep(0.5)
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
