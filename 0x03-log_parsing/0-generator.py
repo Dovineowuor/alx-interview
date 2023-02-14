e9910a5aa0b02061267eddcdf4a087fa330c6801
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-02-15 02:42:04 +0300

    Update 0-generator.py

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,4 +1,3 @@
-# 0-generator.py
 import random
 import time
 
@@ -18,39 +17,3 @@ def generate_log_line():
 while True:
     print(generate_log_line())
     time.sleep(0.5)
-
-# 0-stats.py
-import sys
-import signal
-
-def signal_handler(signal, frame):
-    print("File size: ", total_size)
-    for status, count in sorted(status_codes.items()):
-        if count > 0:
-            print("{}: {}".format(status, count))
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-lines_processed = 0
-
-for line in sys.stdin:
-    lines_processed += 1
-    elements = line.split()
-    try:
-        status = int(elements[-2])
-        size = int(elements[-1])
-    except (ValueError, IndexError):
-        continue
-
-    if status in status_codes:
-        status_codes[status] += 1
-        total_size += size
-
-    if lines_processed % 10 == 0:
-        print("File size: ", total_size)
-        for status, count in sorted(status_codes.items()):
-            if count > 0:
-                print("{}: {}".format(status, count))
