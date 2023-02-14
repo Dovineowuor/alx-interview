324c59aaf904919a13f30c1ae445ae60e4bf958d
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-02-15 02:42:55 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,18 +1,35 @@
-#!/usr/bin/python3
-"""
-Log parsing
-"""
-import random
 import sys
-from time import sleep
-import datetime
+import signal
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+def signal_handler(signal, frame):
+    print("File size: ", total_size)
+    for status, count in sorted(status_codes.items()):
+        if count > 0:
+            print("{}: {}".format(status, count))
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+total_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+lines_processed = 0
+
+for line in sys.stdin:
+    lines_processed += 1
+    elements = line.split()
+    try:
+        status = int(elements[-2])
+        size = int(elements[-1])
+    except (ValueError, IndexError):
+        continue
+
+    if status in status_codes:
+        status_codes[status] += 1
+        total_size += size
+
+    if lines_processed % 10 == 0:
+        print("File size: ", total_size)
+        for status, count in sorted(status_codes.items()):
+            if count > 0:
+                print("{}: {}".format(status, count))
+
