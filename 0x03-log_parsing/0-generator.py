2cafbef3933f5bb67bff68f4698b493ff09a3aec
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-02-15 01:39:44 +0300

    Update 0-generator.py

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,23 +1,34 @@
-#!/usr/bin/python3
-'''A script that generates random HTTP request logs.
-'''
-import random
 import sys
-import datetime
-from time import sleep
+import signal
 
+def signal_handler(signal, frame):
+    print("File size: ", total_file_size)
+    print("Status code metrics:")
+    for status_code in sorted(status_code_counts.keys()):
+        print("{}: {}".format(status_code, status_code_counts[status_code]))
+    sys.exit(0)
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET {} {}\" {} {}\n".format(
-        random.randint(1, 255),
-        random.randint(1, 255),
-        random.randint(1, 255),
-        random.randint(1, 255),
-        datetime.datetime.now(),
-        '/projects/1216',
-        'HTTP/1.1',
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+signal.signal(signal.SIGINT, signal_handler)
+
+total_file_size = 0
+status_code_counts = {}
+line_count = 0
+
+for line in sys.stdin:
+    parts = line.strip().split()
+    if len(parts) != 5:
+        continue
+    status_code = int(parts[4])
+    if status_code not in [200, 301, 400, 401, 403, 404, 405, 500]:
+        continue
+    total_file_size += int(parts[5])
+    line_count += 1
+    if status_code in status_code_counts:
+        status_code_counts[status_code] += 1
+    else:
+        status_code_counts[status_code] = 1
+    if line_count % 10 == 0:
+        print("File size: ", total_file_size)
+        print("Status code metrics:")
+        for status_code in sorted(status_code_counts.keys()):
+            print("{}: {}".format(status_code, status_code_counts[status_code]))
