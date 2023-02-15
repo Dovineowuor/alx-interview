a101cf6d05e1abbe94528880e08be35902ce4969
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-02-15 03:22:57 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,38 @@
-# 0-stats.py
+#!/usr/bin/env python3
 import sys
-from signal import signal, SIGINT
 
-def signal_handler(signal, frame):
-    print("File size: {}".format(file_size))
-    status_codes = sorted(status_codes_dict.keys())
-    for code in status_codes:
-        if status_codes_dict[code] > 0:
-            print("{}: {}".format(code, status_codes_dict[code]))
-    sys.exit(0)
-
-signal(SIGINT, signal_handler)
-
-file_size = 0
-status_codes_dict = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+total_size = 0
 counter = 0
-for line in sys.stdin:
-    items = line.split()
-    if len(items) != 9:
-        continue
-    code = items[-2]
-    if not code.isdigit():
-        continue
-    code = int(code)
-    if code < 200 or code >= 600:
-        continue
-    size = int(items[-1])
-    file_size += size
-    status_codes_dict[str(code)] += 1
-    counter += 1
-    if counter % 10 == 0:
-        print("File size: {}".format(file_size))
-        status_codes = sorted(status_codes_dict.keys())
-        for code in status_codes:
-            if status_codes_dict[code] > 0:
-                print("{}: {}".format(code, status_codes_dict[code]))
+
+try:
+    for line in sys.stdin:
+        elements = line.split()
+        if len(elements) != 9:
+            continue
+
+        try:
+            size = int(elements[-1])
+            code = int(elements[-2])
+        except ValueError:
+            continue
+
+        if code not in status_codes:
+            continue
+
+        total_size += size
+        status_codes[code] += 1
+        counter += 1
+
+        if counter == 10:
+            print("File size: {}".format(total_size))
+            for code in sorted(status_codes.keys()):
+                if status_codes[code] != 0:
+                    print("{}: {}".format(code, status_codes[code]))
+            counter = 0
+except KeyboardInterrupt:
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] != 0:
+            print("{}: {}".format(code, status_codes[code]))
+    sys.exit()
