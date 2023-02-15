54817c4ee51fc225a33deafe86d61ace599a2223
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-02-15 03:12:33 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,15 +1,47 @@
-#!/usr/bin/python3
-import random
+# 0-stats.py
 import sys
-from time import sleep
-import datetime
+from signal import signal, SIGINT
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+def signal_handler(signal, frame):
+    print("File size: {}".format(file_size))
+    status_codes = sorted(status_codes_dict.keys())
+    for code in status_codes:
+        if status_codes_dict[code] > 0:
+            print("{}: {}".format(code, status_codes_dict[code]))
+    sys.exit(0)
+
+signal(SIGINT, signal_handler)
+
+file_size = 0
+status_codes_dict = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
+counter = 0
+for line in sys.stdin:
+    items = line.split()
+    if len(items) != 9:
+        continue
+    code = items[-2]
+    if not code.isdigit():
+        continue
+    code = int(code)
+    if code < 200 or code >= 600:
+        continue
+    size = int(items[-1])
+    file_size += size
+    status_codes_dict[str(code)] += 1
+    counter += 1
+    if counter % 10 == 0:
+        print("File size: {}".format(file_size))
+        status_codes = sorted(status_codes_dict.keys())
+        for code in status_codes:
+            if status_codes_dict[code] > 0:
+                print("{}: {}".format(code, status_codes_dict[code]))
